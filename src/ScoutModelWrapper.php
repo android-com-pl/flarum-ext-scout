da02d704642840730333ad4f683cf3cee81a6f3c
Rafał Całka <calka.rafal@gmail.com>
2024-06-23 14:31:47 +0200

    fix(elasticsearch): return false in `usesSoftDelete` method

--- src/ScoutModelWrapper.php
+++ src/ScoutModelWrapper.php
@@ -23,6 +23,15 @@ class ScoutModelWrapper extends Model
         $this->realModel = $realModel;
     }
 
+    public static function __callStatic($method, $parameters)
+    {
+        if ($method === 'usesSoftDelete') {
+            return self::usesSoftDelete();
+        }
+
+        return (new static)->$method(...$parameters);
+    }
+
     public function getRealModel(): Model
     {
         return $this->realModel;
@@ -200,7 +209,7 @@ class ScoutModelWrapper extends Model
 
     protected static function usesSoftDelete()
     {
-        throw new \Exception('Native Laravel soft delete meta not implemented in Scout for Flarum');
+        return false;
     }
 
     /**
